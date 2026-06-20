868ef6cffccaada480c35d92106c74eaea85c1ec
seanglyhor2222-del <YOUR_GITHUB_EMAIL>
2026-06-20 16:32:01 +0700

    Create admin user

--- build.sh
+++ build.sh
@@ -5,3 +5,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+#!/usr/bin/env bash
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
+python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin','admin@gmail.com','Admin@123')
+"
