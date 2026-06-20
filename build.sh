3123f46d81f98141dc5d647489ba76f99f2f890e
seanglyhor2222-del <YOUR_GITHUB_EMAIL>
2026-06-20 16:45:54 +0700

    Import old data

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ python manage.py collectstatic --noinput
 
 python manage.py migrate
 
+python manage.py loaddata accounts.json
+python manage.py loaddata activities.json
+python manage.py loaddata reports.json
+python manage.py loaddata workflows.json
